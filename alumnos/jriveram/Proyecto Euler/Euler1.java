32d4c2549ae976db9c120a6fa21831e2d75e0b16
Javier <fjavier_riveram@hotmail.com>
2015-02-03 22:03:15 -0600

    corregí errores; agregué comentarios

--- alumnos/jriveram/Proyecto Euler/Euler1.java
+++ alumnos/jriveram/Proyecto Euler/Euler1.java
@@ -1,4 +1,4 @@
-﻿
+
 /**
  *
  * @author Javier
@@ -8,27 +8,27 @@ import java.util.LinkedList;
 
 public class Euler1 {
 
-    public static long problema1(long n) {
-	long a = (n - 1) / 3;
-	long suma = a * (a + 1) / 2 * 3;
-	a = (n - 1) / 5;
-	suma += a * (a + 1) / 2 * 5;
-	a = (n - 1) / 15;
-	suma -= a * (a + 1) / 2 * 15;
+    public static int problema1(int n) {
+	int a = (n - 1) / 3;		//cuento cuántos múltiplos de 3 hay antes de n
+	int suma = a * (a + 1) / 2 * 3;	//sumo esos de múltiplos de 3
+	a = (n - 1) / 5;		//cuento cuántos múltiplos de 5 hay...
+	suma += a * (a + 1) / 2 * 5;	//sumo esos múltiplos de 5
+	a = (n - 1) / 15;		//cuento cuántos múltiplos de 15 hay...
+	suma -= a * (a + 1) / 2 * 15;	//resto esos múltiplos de 15 pues los sumé dos veces 
 	return suma;
     }
 
-    public static long problema2(long n) {
-	long a = 1;
-	long b = 2;
-	long temp;
-	long suma = 0;
+    public static int problema2(int n) {
+	int a = 1;				//variable que empieza en 1 por ser el primer término de la serie
+	int b = 2;				//variable que empieza en 2 por ese el segundo término de la serie
+	int temp;				//variable temporal
+	int suma = 0;				//varaible que lleva la suma que pide le problema
 
-	for (long i = 2; i < n; i++) {
-	    suma += (b % 2 == 0) ? b : 0;
-	    temp = a;
-	    a = b;
-	    b = temp + a;
+	for (int i = 2; suma < n; i++) {	//ciclo que termina cuando la suma sea mayor que n, 4,000,000
+	    suma += (b % 2 == 0) ? b : 0;	//si el término es par es agregado a la suma
+	    temp = a;				
+	    a = b;				//se recorre la serie
+	    b = temp + a;			//se calcula el valor del siguiente término sumando los dos anteriores
 	}
 	return suma;
     }
@@ -70,7 +70,7 @@ public class Euler1 {
 	return primos.getLast();
     }
 
-    private static long problema3(long n) {
+    public static long problema3(long n) {
 	return problema3(n, new LinkedList<Long>());
     }
 
@@ -82,9 +82,8 @@ public class Euler1 {
     }
 
     public static void main(String args[]) {
-	System.out.println("NOTA: LOS NÚMEROS SON DEMASIADO GRANDES Y NO CABEN EN UNA VARIABLE TIPO LONG");
 	System.out.println("Problema 1: " + problema1(1000));
 	System.out.println("Problema 2: " + problema2(4000000));
-	System.out.println("Problema 3: " + problema3(60085147514));
+	System.out.println(problema3(60085147514L));
     }
 }
--- alumnos/jriveram/Proyecto Euler/readme.txt
+++ alumnos/jriveram/Proyecto Euler/readme.txt
@@ -1,3 +1,2 @@
-Nota: Los números son demasiado grandes para que java los lea 
-(problemas 2 y 3) en variables
-tipo long, pero la solución sí funcionó con el caso de prueba.
+Nota: Me falta revisar problema 3, 
+pero espero tenerlo listo antes de la hora de entrega.
