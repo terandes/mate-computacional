ee5d4cf0254c600912d0fc1e539b7fe2089e369e
Javier <fjavier_riveram@hotmail.com>
2015-02-03 23:55:51 -0600

    agregué comentarios, corregí errores; tarea completa

--- alumnos/jriveram/Proyecto Euler/Euler1.java
+++ alumnos/jriveram/Proyecto Euler/Euler1.java
@@ -3,11 +3,11 @@
  *
  * @author Javier
  */
-import static java.lang.Math.sqrt;
 import java.util.LinkedList;
 
 public class Euler1 {
 
+    //método que resuelve el primer problema
     public static int problema1(int n) {
 	int a = (n - 1) / 3;		//cuento cuántos múltiplos de 3 hay antes de n
 	int suma = a * (a + 1) / 2 * 3;	//sumo esos de múltiplos de 3
@@ -18,72 +18,63 @@ public class Euler1 {
 	return suma;
     }
 
+    //método que resuelve el segundo problema
     public static int problema2(int n) {
 	int a = 1;				//variable que empieza en 1 por ser el primer término de la serie
 	int b = 2;				//variable que empieza en 2 por ese el segundo término de la serie
 	int temp;				//variable temporal
 	int suma = 0;				//varaible que lleva la suma que pide le problema
 
-	for (int i = 2; suma < n; i++) {	//ciclo que termina cuando la suma sea mayor que n, 4,000,000
+	while (suma < n) {	//ciclo que termina cuando la suma sea mayor que n, 4,000,000
 	    suma += (b % 2 == 0) ? b : 0;	//si el término es par es agregado a la suma
-	    temp = a;				
+	    temp = a;
 	    a = b;				//se recorre la serie
 	    b = temp + a;			//se calcula el valor del siguiente término sumando los dos anteriores
 	}
 	return suma;
     }
 
-    public static long problema3(long n, LinkedList<Long> primos) {
-	long cota = (long) Math.floor(sqrt(n));
-
-	prueba:
-	for (long i = 2; i <= cota; i++) {
-
-	    for (long x : primos) {
-		if (mcd(x, i) != 1) {
-		    if (n % i == 0) {
-			return problema3(n / i, primos);
-		    }
-		    continue prueba;
-		}
-	    }
-
-	    if (!primos.contains(i)) {
-		primos.add(i);
-	    }
-
-	    if (n % i == 0) {
-		return problema3(n / i, primos);
-	    }
-	}
-
-	for (long x : primos) {
-	    if (mcd(x, n) != 1) {
-		break;
-	    }
-	    if (!primos.contains(n)) {
-		primos.add(n);
-		break;
-	    }
-	}
-
-	return primos.getLast();
-    }
-
-    public static long problema3(long n) {
-	return problema3(n, new LinkedList<Long>());
-    }
-
+    // método calcula el máximo común divisor utilizando el algoritmo de Euclides
     public static long mcd(long m, long n) {
-	if (m == 0) {
+	if (m == 0) {			
 	    return n;
 	}
 	return (mcd(n % m, m));
     }
 
+    //método que obtiene una lista de primos menores a n
+    private static LinkedList<Long> obtenPrimos(long n, LinkedList<Long> primos) {
+	primero:				//etiqueta del primer ciclo
+	for (long i = 2; i <= n; i++) {		
+	    if (!primos.contains(i)) {		//si la lista de primos no contiene i
+		for (long x : primos) {		//para cada primo en la lista:
+		    if (mcd(x, i) != 1) {	//verifica que su mcd sea igual de 1
+			continue primero;	//en caso de ser diferente, significa que es número compuesto, por lo
+		    }				    //que el primer ciclo avanza a la siguiente iteración
+		}
+		primos.addFirst(i);		//si mcd fue igual a 1 para cada número que ya estaba en la lista,
+	    }					    //significa que el número es primo, por lo que se agrega a la lista
+	    if (n % i == 0) {			//si puede dividir entre i, lo divide 
+		return obtenPrimos(n / i, primos);
+	    }
+	}
+	return primos;
+    }
+
+    //método que resuelve el problema 3
+    public static long problema3(long n) {
+	LinkedList<Long> primos = obtenPrimos(n, new LinkedList<Long>());   //obtiene una lista de primos menores a n
+	for (long x : primos) {						    //para cada primo en la lista:
+	    if (n % x == 0) {						    //prueba si es divisor de n
+		return x;						    //si sí, regresa ese primo
+	    }								    //(la lista tiene orden inverso)
+	}
+	return 1;							    //si ninguno lo dividió, regresa 1
+    }
+
     public static void main(String args[]) {
 	System.out.println("Problema 1: " + problema1(1000));
 	System.out.println("Problema 2: " + problema2(4000000));
-	System.out.println(problema3(60085147514L));
+	System.out.println("Problema 3: " + problema3(600851475143L));
     }
 }
--- alumnos/jriveram/Proyecto Euler/readme.txt
+++
@@ -1,2 +0,0 @@
-Nota: Me falta revisar problema 3, 
-pero espero tenerlo listo antes de la hora de entrega.
