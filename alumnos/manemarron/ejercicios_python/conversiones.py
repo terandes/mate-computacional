b78562d87632cd26c197cbc0d2ba39390bb8142f
Jose Manuel Marron <manemarron@gmail.com>
2015-02-17 00:32:10 -0600

    conversiones de hexadecimal a binario y viceversa

--- alumnos/manemarron/ejercicios_python/conversiones.py
+++ alumnos/manemarron/ejercicios_python/conversiones.py
@@ -29,3 +29,51 @@ def binTwosComplementToDecimal(binary):
         return dec
     except Exception as e:
         print e
+
+###### HEXADECIMAL A BINARIO Y VICEVERSA ##############
+HEX_BIN = [
+    ("0000","0"),
+    ("0001","1"),
+    ("0010","2"),
+    ("0011","3"),
+    ("0100","4"),
+    ("0101","5"),
+    ("0110","6"),
+    ("0111","7"),
+    ("1000","8"),
+    ("1001","9"),
+    ("1010","A"),
+    ("1011","B"),
+    ("1100","C"),
+    ("1101","D"),
+    ("1110","E"),
+    ("1111","F"),
+]
+def binaryToHex(binary):
+    """
+    Funcion que convierte de binario a hexadecimal
+    """
+    digits=len(binary)/4
+    if len(binary)%4 != 0: digits += 1
+    binary = "0"*(digits*4-len(binary))+binary
+    hexa=""
+    for i in range(0,digits):
+        aux = binary[4*i:4*(i+1)]
+        j=0
+        for t in HEX_BIN:
+            if t[0] == aux:
+                hexa += t[1]
+                break
+    return hexa
+
+def hexToBinary(hexa):
+    """
+    Funcion que convierte de hexadecimal a binario
+    """
+    binary=""
+    for d in hexa:
+        for t in HEX_BIN:
+            if t[1] == d:
+                binary += t[0]
+                break
+    return binary
